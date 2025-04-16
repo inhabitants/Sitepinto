0baaee063e707d77c644beb1a18d14b50853ba24
inhabitants <borderlessprotocol@gmail.com>
2025-04-16 02:47:29 -0500

    Update Products.tsx

--- src/components/landing/Products.tsx
+++ src/components/landing/Products.tsx
@@ -113,7 +113,7 @@ const Products = () => {
                     {card.buttonText}
                   </a>
                   <a 
-                    href="https://docs.pinto.wtf/docs/pinto-brasil/SubDAO"
+                    href="https://docs.pinto.wtf/docs/SubDAO"
                     className="button-secondary flex-1"
                     target="_blank"
                     rel="noopener noreferrer"
