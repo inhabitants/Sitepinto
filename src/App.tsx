7247276c03b5a0f166a54523ca071ca7af054cee
inhabitants <borderlessprotocol@gmail.com>
2025-04-16 21:24:45 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,25 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+// Reintroduzindo o ThirdwebProvider do pacote antigo
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    {/* ThirdwebProvider dentro do QueryClientProvider */}
+    <ThirdwebProvider activeChain="base">
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThirdwebProvider>
   </QueryClientProvider>
 );
 
